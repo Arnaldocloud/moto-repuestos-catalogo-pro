a5735148eaad513c5edb47ed3e4ffa3660f16ad9
Arnaldocloud <romeroarnaldo1@gmail.com>
2025-04-04 15:22:49 -0400

    client

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE="https://hgzsyjaovltugazgatna.supabase.co";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL="https://hgzsyjaovltugazgatna.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhnenN5amFvdmx0dWdhemdhdG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MjU3NDUsImV4cCI6MjA1OTMwMTc0NX0._UsAJ-AK9nwH85sIr9R4Avha_60j4_OfY9saTEVSD3s";
 
 // Import the supabase client like this:
