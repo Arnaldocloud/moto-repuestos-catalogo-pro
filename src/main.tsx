b9dbbdbae4cff07b30e4ad0b26e928a442b3c261
Arnaldocloud <romeroarnaldo1@gmail.com>
2025-04-04 21:00:45 -0400

    rer

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
-console.log("VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("VITE_SUPABASE_ANON_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 
 
